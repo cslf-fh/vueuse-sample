c9e21a02911bae4f13f16cb383cb4ba03c8822bf
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-11-30 01:49:54 +0900

    [UPDATE] app's url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+const SITE_URL = '/vueuse-sample/';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  app: {
+    baseURL: SITE_URL,
+    cdnURL: `https://cslf-fh.github.io${SITE_URL}`,
+  },
 });
